91c7318ad670510c655bf786b1dfe414df9c85b3
Gabriel Poça <gabrielpoca@gmail.com>
2015-01-15 17:44:41 +0000

    Adds codemirror editor

--- app/client/main.js
+++ app/client/main.js
@@ -1,6 +1,24 @@
+Template.Editor.rendered = function() {
+  this.textarea = this.find('textarea');
+
+  this.editor = CodeMirror.fromTextArea(this.textarea, {
+    lineNumbers: true,
+    mode: 'javascript',
+    theme: 'ambiance'
+  });
+};
+
+Template.Editor.helpers({
+  value: function() {
+    return Session.get('code');
+  }
+});
+
 Template.Editor.events({
-  'click button': function() {
-    var code = $('textarea')[0].value;
+  'click button': function(e, tmp) {
+    tmp.editor.save();
+    var code = tmp.textarea.value;
+
     Meteor.call('submit', code);
   }
 });
